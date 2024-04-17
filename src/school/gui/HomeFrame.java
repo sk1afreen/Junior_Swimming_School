600ee6c9e5a162e8462414c04d6647bce6c2e4d0
sk1afreen <167349699+sk1afreen@users.noreply.github.com>
2024-04-17 23:41:06 +0530

    Created src folder and added  HomeFrame.java

---
+++ src/school/gui/HomeFrame.java
@@ -0,0 +1,74 @@
+package school.gui;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class HomeFrame extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void init() {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					HomeFrame frame = new HomeFrame();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public HomeFrame() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 299, 262);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("HJSS");
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 18));
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setBounds(10, 10, 257, 41);
+		contentPane.add(lblNewLabel);
+		
+		JButton btnAdmin = new JButton("System admin");
+		btnAdmin.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				setVisible(false);
+				AdminLoginFrame.init();
+			}
+		});
+		btnAdmin.setBounds(15, 77, 252, 51);
+		contentPane.add(btnAdmin);
+		
+		JButton btnLearner = new JButton("Learner");
+		btnLearner.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				setVisible(false);
+				LearnerLoginFrame.init();
+			}
+		});
+		btnLearner.setBounds(15, 157, 252, 51);
+		contentPane.add(btnLearner);
+	}
+}
