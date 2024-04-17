62887b7d8330fbec0c0dda205459af6ac91e689b
sk1afreen <167349699+sk1afreen@users.noreply.github.com>
2024-04-17 23:48:32 +0530

    Created src folder and added HomeController.java

---
+++ src/school/controller/HomeController.java
@@ -0,0 +1,16 @@
+package school.controller;
+
+import school.gui.HomeFrame;
+import school.service.RandomGenerator;
+
+public class HomeController {
+	
+	public static void main(String args[]) {
+		RandomGenerator.g5e5n5e5r5a5t5e5D5u5m5my5L5e5a5r5n5e5rs(15);
+		RandomGenerator.ge5n5e5ra5t5e5L5e5s5s5o5n5s(50);
+		RandomGenerator.ge5n5e5ra5te5Co5ac5he5s(4);
+		RandomGenerator.se5t5Le5ss5o5ns5T5o5L5e5a5r5n5er();
+		HomeFrame.init();
+	}
+
+}
